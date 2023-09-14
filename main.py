6c4bb96bae6ebb94fa03e60984bf643f5f0a361d
MariAssisR <mari@gersonramos.com>
2023-09-14 09:06:20 -0300

    code done. ready to organize

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# AUX
+# get a new deck and suffle it
 def shuffleCardsAndGetDeck():
     shuffle_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 
@@ -9,7 +9,7 @@ def shuffleCardsAndGetDeck():
         response = requests.get(shuffle_url)
         if response.status_code == 200:
             data = json.loads(response.text)
-            #get the id of the deck
+            # get the id of the deck
             data_deck_id =  data["deck_id"]
             return data_deck_id
         else:
@@ -17,14 +17,15 @@ def shuffleCardsAndGetDeck():
     except:
         print("HTTP request error in function shuffleCardsAndGetDeck\n")
         pass
-        
+
+# draw a count number of cards from deck_id
 def dealCards(deck_id, count):
     draw_card_url = f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count={count}"
     try:
         response = requests.get(draw_card_url)
         if response.status_code == 200:
             data = json.loads(response.text)
-            #get the count of cards of the deck with id equal deck_id
+            # get the count of cards of the deck with id equal deck_id
             data_cards = data["cards"]
             return data_cards
         else:
@@ -33,6 +34,7 @@ def dealCards(deck_id, count):
         print("HTTP request error in function dealCards\n")
         pass
 
+# get raw data of cards and return only the value and the suit of it
 def arrangeCardsFromApi(raw_cards_data):
     mapping_value = {
         "ACE": 1,
@@ -55,35 +57,84 @@ def arrangeCardsFromApi(raw_cards_data):
         if value in mapping_value:
             value = mapping_value[value]
 
-        if suit in mapping_suit:
-            suit = mapping_suit[suit]
-
-        cards.append({"value": value,"suit": suit})
+        cards.append({"value": value,"suit": mapping_suit[suit]})
 
     return cards
 
-def orderCards():
-    pass
+# order the cards in order of: first=suit and second=value
+def orderCards(card):
+    order_suit = {"paus": 0, "ouros": 1, "copas": 2, "espadas": 3}
+    return (order_suit[card["suit"]], int(card["value"]))
 
-def checkForSequence():
-    pass
+# return if a hand of ordered cards has a sequence
+# sequence = 3 or more consecutive cards 
+def checkForSequence(cards):
+    for index in range(len(cards) - 2):
+        card1 = cards[index]
+        card2 = cards[index + 1]
+        card3 = cards[index + 2]
 
-def findWinner():
-    pass
+        if(card1["suit"] == card2["suit"] == card3["suit"]):
+            if(int(card1["value"]) == int(card2["value"])-1 == int(card3["value"])-2):
+                return True
+    return False
+
+# prepare cards in list to output
+def outputCards(cards):
+    output_cards = []
+    for card in cards:
+        output_cards.append(str(card["value"])+" de "+card["suit"])
+    return output_cards
+
+def findWinner(alan_cards, bruno_cards):
+    result = {}
+    alan_sequence = checkForSequence(alan_cards)
+    result["alan"] = {
+        "cartas": outputCards(alan_cards),
+        "tem_sequencia": alan_sequence
+    }
+    bruno_sequence = checkForSequence(bruno_cards)
+    result["bruno"] = {
+        "cartas": outputCards(bruno_cards),
+        "tem_sequencia": bruno_sequence
+    }
+
+    if(alan_sequence and not bruno_sequence):
+       result["vencedor"] = "alan"
+    elif(bruno_sequence and not alan_sequence):
+        result["vencedor"] = "bruno"
+    else:
+        result["vencedor"] = "empate"
+
+    return result
 
 ############################################################
-deck_id = shuffleCardsAndGetDeck()
-alan_unarranged_cards = dealCards(deck_id,10)
-print(alan_unarranged_cards)
-alan_cards = arrangeCardsFromApi(alan_unarranged_cards)
-print(alan_cards)
-for card in alan_cards:
-    print(card["value"])
-    print("\n")
 
 # MAIN
 # start game
 # shuffle, deal cards, order cards, check for sequence, output
 # play again?
+
+# get new deck and suffle
+deck_id = shuffleCardsAndGetDeck()
+
+# get alan cards
+alan_unarranged_cards = dealCards(deck_id,11)
+alan_unordered_cards = arrangeCardsFromApi(alan_unarranged_cards)
+alan_cards = sorted(alan_unordered_cards, key=orderCards)
+
+# get bruno cards
+bruno_unarranged_cards = dealCards(deck_id,11)
+bruno_unordered_cards = arrangeCardsFromApi(bruno_unarranged_cards)
+bruno_cards = sorted(bruno_unordered_cards, key=orderCards)
+
+# find and print winner
+output = findWinner(alan_cards, bruno_cards)
+output_json = json.dumps(output, indent=4)
+print(output_json)
+
+
+
+
 
 
